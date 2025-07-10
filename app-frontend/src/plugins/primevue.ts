483c7a490e83bf255b6cd8cad39bc5fd3391f7d1
Diego Alvarez S. <dialvarezs@gmail.com>
2025-07-10 01:23:47 -0300

    fix: Minor

--- app-frontend/src/plugins/primevue.ts
+++ app-frontend/src/plugins/primevue.ts
@@ -1,7 +1,7 @@
 import PrimeVue from 'primevue/config'
 import type { App } from 'vue'
 import Aura from '@primeuix/themes/aura'
-import ConfirmationService from 'primevue/confirmationservice';
+import ConfirmationService from 'primevue/confirmationservice'
 
 
 export default function setupPrimeVue(app: App) {
